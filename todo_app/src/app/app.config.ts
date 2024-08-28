483c13cd54737ef793d09cce9a0423397ae7c7a5
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-08-28 07:53:24 +0530

    vercel host prep

--- todo_app/src/app/app.config.ts
+++ todo_app/src/app/app.config.ts
@@ -2,6 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
+
+console.log(environment.production); // true or false
+console.log(environment.apiUrl); // 'http://my-prod-url' or 'http://my-dev-url'
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
---
+++ todo_app/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.development.ts
+export const environment = {
+    production: false,
+    apiUrl: 'http://my-dev-url'
+  };
+  
